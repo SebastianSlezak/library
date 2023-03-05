410a99f890a0ee51673558c1d0a6768aa24d52af
SebastianSlezak <szalassje@gmail.com>
2023-03-05 23:58:02 +0100

    version 0.8 - Loops have been added in order to reselect options

---
+++ src/app/LibraryApp.java
@@ -0,0 +1,8 @@
+package app;
+
+public class LibraryApp {
+    public static void main(String[] args) {
+        LibraryControl libControl = new LibraryControl();
+        libControl.controlLoop();
+    }
+}
